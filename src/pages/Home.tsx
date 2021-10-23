59175c0b1d61bb8b45de5fcbc681588c4dc37cb1
Gabriel Borges <gbdesigns13@gmail.com>
2021-10-23 20:31:23 -0300

    Desafio finalizado 🚀

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,28 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
-  }
+    setTasks(oldTasks => [...oldTasks, {
+      id: new Date().getTime(),
+      done: false,
+      title: newTaskTitle
+    }]);
+  };
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    setTasks(oldTasks => oldTasks.map(task => {
+      if(task.id === id) {
+        return {
+          ...task,
+          done: !task.done
+        }
+      } else {
+        return task
+      }
+    }))
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(oldTasks => (oldTasks.filter(task => task.id !== id)))
   }
 
   return (
